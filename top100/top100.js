f5e43b7c9615238dea99168249b045ae6a0be23d
prashgde <prashgde@gmail.com>
2021-11-12 15:11:11 +0530

    Selecting only movies

--- top100/top100.js
+++ top100/top100.js
@@ -11,7 +11,7 @@ require('dotenv').config();
         const moviesWithRequiredFields = [];
         const readFile = util.promisify(fs.readFile);
         const writeFile = util.promisify(fs.writeFile);
-        const allMoviesTSV = await readFile('top100.tsv', 'utf-8');
+        const allMoviesTSV = await readFile('top300.tsv', 'utf-8');
         const allMovies = tsv2json(allMoviesTSV);
         let allMovieIds = allMovies.map(movie => movie[0]);
 
@@ -22,18 +22,26 @@ require('dotenv').config();
         }
 
         let id = 0;
+        let count = 0;
+
         for (const movie of movies) {
-            if (movie.data.Title && movie.data.Year && movie.data.Genre && movie.data.imdbRating) {
-                moviesWithRequiredFields.push({
-                    name: movie.data.Title,
-                    year: movie.data.Year,
-                    genre: movie.data.Genre,
-                    rating: movie.data.imdbRating,
-                    id: id++
-                });
-            }
+            count++;
+            if (id === 100)
+                break;
+            if (movie.data.Type)
+                if (movie.data.Type === 'movie')
+                    if (movie.data.Title && movie.data.Year && movie.data.Genre && movie.data.imdbRating) {
+                        moviesWithRequiredFields.push({
+                            name: movie.data.Title,
+                            year: movie.data.Year,
+                            genre: movie.data.Genre,
+                            rating: movie.data.imdbRating,
+                            id: id++
+                        });
+                    }
         }
 
+        console.log(`Searched ${count}/300 top movies`);
         await writeFile('top100.json', JSON.stringify(moviesWithRequiredFields, null, 4));
     } catch (err) {
         console.log(err);
